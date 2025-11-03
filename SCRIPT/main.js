eaebf8772676d937489d214f3a6d8d4c594e4bc4
Juan <jpaezvillaveses@gmail.com>
2025-11-03 13:03:07 -0500

    Finalizado formulario de contacto

--- SCRIPT/main.js
+++ SCRIPT/main.js
@@ -34,3 +34,44 @@ botones.forEach(boton => {
     });
   });
 });
+
+const form = document.getElementById('formContact');
+const errorMsg = document.getElementById('errorMsg');
+
+form.addEventListener('submit', function(e) {
+  e.preventDefault();
+  
+  const nombre = document.getElementById('nombre').value.trim();
+  const email = document.getElementById('email').value.trim();
+  const mensaje = document.getElementById('mensaje').value.trim();
+  const mensajeExito = document.getElementById('msg');
+  let errores = [];
+
+  if (nombre === '') {
+    errores.push('El nombre es obligatorio.');
+  }
+  if (email === '') {
+    errores.push('El correo electrónico es obligatorio.');
+  }
+  if (mensaje === '') {
+    errores.push('El mensaje es obligatorio.');
+  }
+  if (errores.length > 0) {
+    errorMsg.innerHTML = errores.join('<br>');
+    errorMsg.classList.remove('d-none');
+  } else {
+    errorMsg.classList.add('d-none');
+    localStorage.setItem('mensajeExito', '¡Mensaje enviado con éxito!');
+    form.submit();
+    
+  }
+});
+
+window.addEventListener('load', () => {
+  const mensajeGuardado = localStorage.getItem('mensajeExito');
+  if (mensajeGuardado) {
+    const mensajeExito = document.getElementById('msg');
+    mensajeExito.textContent = mensajeGuardado;
+    localStorage.removeItem('mensajeExito');
+  }
+});
